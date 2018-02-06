60b896bc01c83ebf470605464a4b757defbebeda
Ken MacLean <kmaclean@voxforge.org>
2018-02-06 13:52:19 -0500

    draft choosing one of multiple prompt files to select random prompts from

--- assets/static/scripts/prompts.js
+++ assets/static/scripts/prompts.js
@@ -18,6 +18,85 @@
 // #############################################################################
 
 /**
+* verify that read.md entries contain valid data
+*/
+Prompts.validate_Readmd_file = function () {
+  var variable_list = ['page_language', 
+                    'page_prompt_list_contains_id', 
+                    'page_prompt_list_files', 
+                    'page_total_number_of_prompts'];
+  for (var i = 0; i < variable_list.length; i++) {
+    if (typeof variable_list[i] === 'undefined') {
+      console.log("ERROR: " + variable_list + " not defined in read.md for language: " + 
+                  page_language);
+    }
+  }
+  // validate contents of page_prompt_list_files array
+  for (var i = 0; i < page_prompt_list_files.length; i++) {
+    // check for undefined fields
+    if (typeof page_prompt_list_files[i].id === 'undefined') {
+      console.log("ERROR: prompt_list_files[" + i + "].id not defined in read.md for language: " + 
+                  page_language);
+    }
+    if (typeof page_prompt_list_files[i].start === 'undefined') {
+      console.log("ERROR: prompt_list_files[" + i + "].start not defined in read.md for language: " + 
+                  page_language);
+    }
+    if (typeof page_prompt_list_files[i].file_location === 'undefined') {
+      console.log("ERROR: prompt_list_files[" + i + "].file_location not defined in read.md for language: " + 
+                  page_language);
+    }
+    if (typeof page_prompt_list_files[i].end === 'undefined') {
+      console.log("ERROR: prompt_list_files[" + i + "].end not defined in read.md for language: " + 
+                  page_language);
+    }
+    // numeric validations
+    if (page_prompt_list_files[i].start <  0 || page_prompt_list_files[i].start > page_total_number_of_prompts) {
+      console.log("ERROR: prompt_list_files[" + i + "].start outside valid range read.md for language: " + 
+                  page_language);
+    }
+    if (page_prompt_list_files[i].end <  0 || page_prompt_list_files[i].end > page_total_number_of_prompts) {
+      console.log("ERROR: prompt_list_files[" + i + "].start outside valid range read.md for language: " + 
+                  page_language);
+    }
+    // check against previous prompt file entry
+    if (i-1 >= 0) {
+      if (page_prompt_list_files[i].start <=  page_prompt_list_files[i-1].end) {
+        console.log("ERROR: prompt_list_files[" + i + "].start greater than prompt_list_files[" + (i-1) + "].end read.md for language: " + 
+                    page_language);
+      }
+    }
+    // check against next prompt file entry
+    if (i+1 < page_prompt_list_files.length) {
+      if (page_prompt_list_files[i].end >=  page_prompt_list_files[i+1].start) {
+        console.log("ERROR: prompt_list_files[" + i + "].end greater than prompt_list_files[" + (i+1) + "].start read.md for language: " + 
+                    page_language);
+      }
+    }
+  }
+
+  var last_prompt_file = page_prompt_list_files.length-1;
+  if (page_prompt_list_files[last_prompt_file].end > page_total_number_of_prompts) {
+    console.log("ERROR: prompt_list_files[" + i + "].end should be less than or equal to page_total_number_of_prompts in read.md for language: " + 
+                page_language);
+  }
+}
+
+/**
+* get number of prompt files defined in language's read.md file
+* Jekyll front-matter uses YAML to define data structures... prompt_list_files
+* in read.md file gets converted to an array.
+*/
+Prompts.get_promptFile_count = function () {
+  if (typeof page_prompt_list_files.length === 'undefined') {
+    console.log("WARNING: page_prompt_list_files.length not defined in read.md for language: " + 
+                page_language);
+  }
+
+  return page_prompt_list_files.length;
+}
+
+/**
 * Class declaration
 */
 function Prompts () {
@@ -28,7 +107,13 @@ function Prompts () {
   this.prompt_count = 0; // number of prompts read
   this.prompts_recorded = []; // list of prompts that have been recorded
 
-  this.index = Math.floor((Math.random() * page_total_number_of_prompts) + 1);
+  /* inner functions */
+
+  Prompts.validate_Readmd_file();
+
+  this.random_prompt_file = Math.floor((Math.random() * Prompts.get_promptFile_count())); // zero indexed
+  console.log("random_prompt_file= " + this.random_prompt_file);
+  console.log("start prompt id= " + page_prompt_list_files[this.random_prompt_file].start);
 }
 
 /**
@@ -37,15 +122,10 @@ function Prompts () {
 var prompts = new Prompts();
 
 /**
-* reset prompt array and index after submission is completed
-*/
-Prompts.promptFile_count = function () {
-  return page_prompt_list_files.length;
-}
-
-/**
-TODO make sure that first and last lines dont have extra html characters (e.g. "<pre><p> at beginngig or </pre>" at end...
 * callback for jquery get to process the received prompts file
+*
+* see https://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
+* prototype methods will not work with callback???
 */
 Prompts.processPromptsFile = function (prompt_data) {
     function pad (num, size) {
@@ -60,72 +140,28 @@ Prompts.processPromptsFile = function (prompt_data) {
     { // first word of prompt line is the prompt ID
       prompts.list[i] = sentences[i];
     } else {
-      var prompt_id = page_language + pad(i,4);
-      prompts.list[i] = prompt_id  + " " + sentences[i];
-    }
-  }
-}
-
-// TODO add assert to validate prompt files line counts to make sure they are same as in Markdown
-// TODO add asserts to prompt file start and end locations
-
-prompt.index = 600;
-var prompt_url;
-var i=0;
-if (Prompts.promptFile_count == 1) {
-  $.get(page_prompt_list_files[i]['file_location'], 
-             Prompts.processPromptsFile );
-} else if (Prompts.promptFile_count == 2) {
-  var text_of_first_file = "";
-  var text_of_second_file = "";
-  var promise1 = $.get(page_prompt_list_files[i]['file_location'], 
-    function( data ) {
-        text_of_first_file = data;
-  });
-   var promise2 = $.get(page_prompt_list_files[i+1]['file_location'], 
-    function( data ) {
-        text_of_second_file = data;
-  });
-  // wait for gets to return to merge the files and call prompt processing
-  Promise.all([promise1, promise2]).then(function(values) {
-    var merged_text_files = text_of_first_file + text_of_second_file;
-    Prompts.processPromptsFile( merged_text_files );
-  }
-} else {
-  while ( (prompt.index + max_num_prompts) < page_prompt_list_files[i]['end'] ) {
-    i++;
-  }
-  prompt_url = page_prompt_list_files[i]['file_location'];
-} else {
-  console.log("Error in Jekyll Read.md file");
-}
-
-
-/**
-* see https://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
-* Note: this is a synchronous request...
-
-jQuery.get('/assets/static/prompts/' + prompts.filename, function(data) {
-  function pad (num, size) {
-    var s = num+"";
-    while (s.length <= size) s = "0" + s;
-    return s;
-  }
-
-  var sentences = data.split('\n');
-  for (var i = 0; i < sentences.length; i++) {
-    if (page_prompt_list_contains_id)
-    { // first word of prompt line is the prompt ID
-      prompts.list[i] = sentences[i];
-    } else {
-      var prompt_id = prompts.language + pad(i,4);
+      var prompt_id = page_language + pad( i + page_prompt_list_files[prompts.random_prompt_file].start, 4 );
       prompts.list[i] = prompt_id  + " " + sentences[i];
     }
   }
 
-  prompts.index = Math.floor((Math.random() * prompts.list.length) + 1);
+  var num_of_prompts = page_prompt_list_files[prompts.random_prompt_file].end - page_prompt_list_files[prompts.random_prompt_file].start;
+  if (num_of_prompts !==  sentences.length) {
+    console.log("Warning: number of prompts in prompt_list_files[" + prompts.random_prompt_file + "] (end - start) in read.md not same as prompt file line counts for language: " + 
+                page_language);
+  }
+
+  // set random index of prompt line to present to user
+  prompts.index = Math.floor((Math.random() * prompts.list.length) + 1); // one indexed
+}
+
+/* synchronous request... */
+$.get(page_prompt_list_files[prompts.random_prompt_file]['file_location'], 
+      Prompts.processPromptsFile
+).fail(function() {
+  console.log("Error: cannot find prompts file on VoxForge server: " + 
+              page_prompt_list_files[prompts.random_prompt_file]['file_location']);
 });
-*/
 
 /**
 * updates the current number of prompts that the user selected from dropdown
@@ -142,11 +178,15 @@ $('#max_num_prompts').click(function () {
 * ### METHODS ##############################################
 */
 
+
+
+
+
 /**
 * reset prompt array and index after submission is completed
 */
 Prompts.prototype.resetIndices = function () {
-  this.index = Math.floor((Math.random() * prompts.list.length) + 1);
+  this.index = Math.floor((Math.random() * prompts.list.length) + 1); // one indexed
   this.prompt_count = 0; // number of prompts read
   this.prompts_recorded = []; // list of prompts that have been recorded
 }
