d17d3da49e1dac476a9df5dd5423317f14dc8241
Ken MacLean <kmaclean@voxforge.org>
2018-03-07 16:07:40 -0500

    draft implementation of FSM

---
+++ assets/static/scripts/appFSM.js
@@ -0,0 +1,199 @@
+/*
+Many thanks to Peter Warden for the open-speech-recording app
+(https://github.com/petewarden/open-speech-recording) that was used
+as a starting point.
+
+Copyright 2018 VoxForge
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+https://www.acunetix.com/websitesecurity/cross-site-scripting/
+XSS - How Cross-site Scripting works
+In order to run malicious JavaScript code in a victim’s browser, 
+an attacker must first find a way to inject a payload into a web page that 
+the victim visits. 
+In order for an XSS attack to take place the vulnerable website needs 
+to ***directly include user input in its pages***. An attacker can then 
+insert a string that will be used within the web page and treated as
+code by the victim’s browser.
+This app does not display other users' input in its pages, so no obvious XSS 
+vulnerability...
+
+TODO: CSRF - Cross site request forgery
+
+// for testing with Chrome: requires https; can bypass this with:
+// google-chrome --user-data-dir=~/temp --unsafely-treat-insecure-origin-as-secure="http://flask.voxforge1.org"
+// need Google Chrome version > 58 for wavesurfer to work correctly
+*/
+
+// #############################################################################
+
+// display variables
+var record = document.querySelector('.record');
+var stop = document.querySelector('.stop');
+var upload = document.querySelector('.upload');
+var soundClips = document.querySelector('.sound-clips');
+var canvas = document.querySelector('.visualizer');
+// constants
+var RECORDING_TIMEOUT = 15000; // 15 seconds
+var RECORDING_TIMEOUT = 2000; // 15 seconds
+
+// finite state machine object
+var fsm;
+
+/**
+* Finite State Machine
+*/
+function setUpFSM() {
+  var timeout_obj;
+
+  fsm = new StateMachine({
+    init: 'waveformdisplay',
+    transitions: [
+      { name: 'recordclicked',       from: 'waveformdisplay',          to: 'recording' },
+      { name: 'stopclicked',         from: 'recording',                to: 'waveformdisplay'  },
+      { name: 'recordingtimeout',    from: 'recording',                to: 'waveformdisplay' },  
+      { name: 'uploadclicked',       from: 'maxpromptwaveformdisplay', to: 'uploading' },
+      { name: 'maxpromptsreached',   from: 'waveformdisplay',          to: 'maxpromptwaveformdisplay' }, 
+      { name: 'maxpromptsreached',   from: 'maxpromptwaveformdisplay', to: 'messagetoupload' },   
+      { name: 'yesuploadmessage',    from: 'messagetoupload',          to: 'uploading' },
+      { name: 'canceluploadmessage', from: 'messagetoupload',          to: 'maxpromptwaveformdisplay' },
+      { name: 'deleteclicked',       from: 'waveformdisplay',          to: 'waveformdisplay'  },
+      { name: 'deleteclicked',       from: 'maxpromptwaveformdisplay', to: 'waveformdisplay'  },
+    ],
+    methods: {
+      // Transition Actions: user initiated
+      onStopclicked: function() { 
+        stopClicked();
+        clearTimeout(timeout_obj);
+      },
+      onUploadclicked: function() { uploading() },
+      onDeleteclicked: function() { 
+        record.disabled = false;
+        updateProgress();
+      },
+
+      // Transition Actions: system initiated
+      onRecordingtimeout: function() { 
+        endRecording();
+        console.log("recorder stopped");
+        record.style.background = "";
+        record.style.color = "";
+      },
+      onMaxpromptsreached: function() { console.log('onmaxpromptsreached')  },
+      onYesuploadmessage: function() { console.log('onyesuploadmessage')  },
+      onCanceluploadmessage: function() { console.log('oncanceluploadmessage')  },
+
+      // States Actions: on entry
+      onWaveformdisplay: function() { 
+        record.disabled = false;
+        stop.disabled = true;
+        upload.disabled = true;
+        console.log('onWaveformdisplay: waiting for user input') 
+      },
+
+      onRecording: function() { 
+        record.disabled = true;
+        var prompt = prompts.getNextPrompt();
+        if (prompt !== null) {
+          stop.disabled = false;
+          recording(prompt);
+
+          timeout_obj = setTimeout(function(){
+            fsm.recordingtimeout();
+          }, RECORDING_TIMEOUT);
+        } else {
+          stop.disabled = true;
+          messageToUpload();
+        }
+      },
+
+      onMaxpromptsreached:   function() { 
+        // to give browser enough time to process the last audio recording
+        setTimeout( function () {
+          messageToUpload();
+          return;
+        }, 300); 
+      },
+
+      onMaxpromptwaveformdisplay:   function() { console.log('onmaxpromptwaveformdisplay') },
+
+      onMessagetoupload: function() { 
+        messageToUpload();
+        upload.disabled = false;
+        record.disabled = true;
+      },
+
+      onUploading: function() { 
+        console.log('onUploading');
+        upload.disabled = true;
+        record.disabled = false;
+        uploading();
+      }
+    }
+  });
+
+  record.onclick = function() { fsm.recordclicked() }
+  stop.onclick = function() { 
+    fsm.stopclicked();
+    stop.disabled = true;
+    record.disabled = false;
+  }
+  upload.onclick = function() { fsm.upload() }
+}
+
+
+/**
+* update number of prompts recorded and total number of prompts to record
+*/
+function updateProgress() {
+  var progress = prompts.getProgressDescription();
+  document.querySelector('.progress-display').innerText = progress;
+}
+
+/**
+* display window to ask user if they want to upload their recordings to 
+* VoxForge server
+*/
+function messageToUpload() {
+  if (confirm('Are you ready to upload your submission?\nIf not, press cancel now,' + 
+	      ' and then press Upload once you are ready.')) {
+    fsm.yesuploadmessage();
+  } 
+  fsm.canceluploadmessage();
+}
+
+/**
+* user has clicked stop... disconnect recording audio nodes
+*
+* the actual stopping of recording is delayed because some users hit it early
+* and cut off the end of their recording
+*/
+function stopClicked() {
+  setTimeout( function () {
+    $('.info-display').hide();
+
+    endRecording();
+
+    record.style.background = "";
+    record.style.color = ""; 
+
+    if ( prompts.maxPromptsReached() ) {
+      fsm.maxpromptsreached();
+
+    }
+  }, 400);
+}
---
+++ assets/static/scripts/uploading.js
@@ -0,0 +1,130 @@
+
+
+// zip and upload Web Worker
+var zip_worker = new Worker('/assets/static/scripts/ZipWorker.js');
+
+/**
+* if page reloaded kill background worker threads before page reload
+* to prevent zombie worker threads in FireFox
+*/
+$( window ).unload(function() {
+  zip_worker.terminate();
+});
+
+/**
+* collect all recorded audio into an array (audioArray) then calls function 
+* that calls web worker that actually creates the zip file for download
+* to VoxForge server
+*/
+function uploading() {
+  var allClips = document.querySelectorAll('.clip');
+  var clipIndex = 0;
+  var audioArray = [];
+
+  function audioArrayLoop() {
+    var clip = allClips[clipIndex];
+    clip.style.display = 'None';
+    var audioBlobUrl = clip.querySelector('audio').src;
+    var prompt = clip.querySelector('prompt').innerText;
+    var prompt_id = prompt.split(/(\s+)/).shift();
+    prompts.prompts_recorded[clipIndex] = prompt + '\n';
+
+    // Ajax is asynchronous - once the request is sent script will 
+    // continue executing without waiting for the response.
+    var xhr = new XMLHttpRequest();
+    // get blob from browser memory; 
+    xhr.open('GET', audioBlobUrl, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        var blob = this.response;
+        // add current audio blob to zip file in browser memory
+        audioArray.push ({
+            filename: prompt_id + '.wav', 
+            audioBlob: blob
+        });
+        clipIndex += 1;
+        if (clipIndex < allClips.length) {
+          audioArrayLoop();
+        } else {
+          // must be called here because ajax is asynchronous
+          // Q1: why doesnt createZipFile get called many times as the call stack unrolls???
+          // ... because status no longer status == 200???
+          createZipFile(audioArray);
+        }
+      }
+    };
+    xhr.send();
+    console.log('===done audioToArray===');
+  }
+  
+  audioArrayLoop();
+}
+
+/**
+* call web worker to create zip file and upload to VoxForge server
+*/
+function createZipFile(audioArray) {
+  zip_worker.onmessage = zipworkerDone;
+
+  var readme_blob = new Blob(profile.toArray(), {type: "text/plain;charset=utf-8"});
+  var prompts_blob = new Blob(prompts.toArray(), {type: "text/plain;charset=utf-8"});
+  var profile_json_blob = new Blob([profile.toJsonString()], {type: "text/plain;charset=utf-8"});
+  var prompts_json_blob = new Blob([prompts.toJsonString()], {type: "text/plain;charset=utf-8"});
+  zip_worker.postMessage({
+    command: 'zipAndUpload',
+    username: profile.getUserName(),
+    language: page_language,
+    temp_submission_name: profile.getTempSubmissionName(),
+    readme_blob: readme_blob,
+    prompts_blob: prompts_blob,
+    profile_json_blob: profile_json_blob,
+    prompts_json_blob: prompts_json_blob,
+    audio: audioArray,
+  });
+
+  /** 
+  * display upload to VoxForge server status to user
+  */
+  function showUploadStatus(message) {
+    $('#upload_status_display').show();
+    $('#upload_status_display').text(message);
+    $('#upload_status_display').css({ 'color': 'green', 'font-size': '50%' });
+    setTimeout( function () {
+      //document.querySelector('.upload_status_display').innerText = "";
+      $('#upload_status_display').hide();
+      return;
+    }, 3000);
+  }
+
+  /**
+  * receives replies from work thread and displays status accordingly
+  *
+  * this is a worker callback inside the worker context
+  */
+  function zipworkerDone(event) { 
+    if (event.data.status === "transferComplete") {
+      console.log('message from worker: Upload to VoxForge server completed');
+      showUploadStatus("Upload successfull!");
+    } else if (event.data.status === "savedInBrowserStorage") {
+      console.log('message from worker: problem with Internet connection, submission saved in browser storage');
+      alert("No Internet connection, submission saved in browser storage.  \nIt will be uploaded next time you make a submission with Internet up.");
+    } else if (event.data.status === "foundSavedFailedUploads") {
+      console.log('message from worker: found submissions saved to browser, uploading them...');
+      showUploadStatus("Found saved submission(s), uploading to VoxForge server.");
+    } else {
+      console.log('message from worker: transfer error: ' + event.data.status);
+    }
+  };
+
+  document.cookie = 'all_done=true; path=/';
+  profile.addProfile2LocalStorage();
+
+  prompts.resetIndices();
+  $( '.sound-clips' ).empty();
+  clip_id = 0;
+
+  console.log('===done allDone===');
+}
+
+
