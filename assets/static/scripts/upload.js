1d0c98a062158270d2dbfc5116815bfd2c4634c8
Ken MacLean <kmaclean@voxforge.org>
2018-04-09 13:58:23 -0400

    move to prod

--- assets/static/scripts/upload.js
+++ assets/static/scripts/upload.js
@@ -145,12 +145,10 @@ function upload( when_audio_processing_completed_func ) {
       when_audio_processing_completed_func(); 
     }
 
-
     /**
-    *
     * for testing CORS make sure you have rootCA cert installed
     * on browser to be tested (Linux, Android, Unix...)
-    * otherwise operation will fail silently
+    * otherwise operation will fail (silently on Android...)
 
     * FireFox: TypeError: swRegistration.sync is undefined
     * background sync is not supported in FireFox
@@ -160,11 +158,9 @@ function upload( when_audio_processing_completed_func ) {
     *
     * - Chrome on Linux and Windows 10 supports service workers for fetching
     * and background sync; 
-    * - Chrome on Android 5 - works with service workers
-    * - Chrome on Android 4.4.2 - needs valid root certificate installed
-    * in browser for local testing; but in prod, where we are using
-    * cors where each domain has different ssl certificate; only web
-    * worker seems to work, service worker fails silently...
+    * - Chrome on Android >5 - works with service workers
+    * - Chrome on Android 4.4.2 - move to canadian hoster has SSL certificate 
+    * that now works with 4.4.2....
     * - Firefox on Linux & Windows 10 supports service workers for fetching 
     * but not background sync, therefore use Web Worker for uploads; 
     * FireFox works on Andoid 4.4.2 - now needs a root certificate
@@ -177,160 +173,47 @@ function upload( when_audio_processing_completed_func ) {
     */
 
     /** 
-    * worker Processing - depending on browser support, use background sync and 
-    * a service worker to upload submission, use a web worker that uploads in 
-    * background; or perform a synchronous upload neither is supported
-
+    * worker Processing - depending on browser support, use service worker and 
+    * background sync to upload submission, if not available, use a web
+    * worker that uploads in background; or 
+    * TODO perform a synchronous upload
+    * if neither is supported... which makes no sense since using web workers
+    * to record and zip submission....
+    *
     * service worker - background sync (e.g. current Chrome on Linux/Win10/Android 5 and up)
     * swRegistration.sync.register: requests a one-off sync to upload the saved 
     * submission.  It will upload if there is connectivity, if there is none, 
     * it will keep on trying to upload until connectivity is made
     * and then will delete saved submission from browser storage
-
-    * Second call to sync takes longer than you think, but it will work
-    * eventually... service worker seems to collect up all the calls to
-    * service worker and does uploads consecutively
-    * wait about 1-2 minutes
     *
     * web worker - background upload  (e.g. current Firefox on Linux/Win10/Android 4.4.2 and up)
     * some browsers implement service workers for caching files but not 
-    * background sync... therefore use Web Worker to upload submission and 
-    * upload any previously saved submissions (in browser storage)
+    * background sync (e.g. Firefox), therefore use Web Worker to upload 
+    * submission and upload any previously saved submissions (in browser 
+    * storage)
     */
-
-        // Chrome Android 4.4.2 fails silently when using service worker...
-        // force use of webworker for background upload
-/**
-1) from jekyll_voxforge.org with upload to: jekyll_voxforge.org
-1.1.1) so when using Chrome Android 4.4.2 with only a public certicate and no 
-corresponding root certificate stored on Android (Security>Credential Storage>Trusted Credentials
-get following browser error when trying to run service worker:
-    An SSL certificate error occurred when fetching the script.
-    Failed to load resource: net::ERR_CERT_AUTHORITY_INVALID /voxforge_sw.js 
-    ServiceWorker registration failed:  DOMException: Failed to register 
-    a ServiceWorker: An SSL certificate error occurred when fetching the script. app.js:299 
-1.1.2) works OK with WebWorker
-1.2) Chrome Android 4.4.2 with rootCA installed on browser and service worker
-works!
-
-2.1) try on voxforge.github.io, service worker does not work, get:
-    Uncaught (in promise) Request failed voxforge_sw.js:1
-
-2.3) need to try with webworker....
-
-2.2) try with asyncMainThreadUpload, get error:
-    POST https://upload.voxforge1.org/ net::ERR_CERT_AUTHORITY_INVALID  processSavedSubmissions.js:76
-
-- try with rootCA installed on browser and web worker
-- try with no rootCA and web worker
-
-- then try CORS (from jekyll_voxforge.org with upload to: jekyll2_voxforge.org)
-for all of these...
-
-More testing:
-first error:
-An SSL certificate error occurred when fetching the script.
-Failed to load resource: net::ERR_CERT_AUTHORITY_INVALID /voxforge_sw.js 
-
-added header to index.php:
-header("Service-Worker-Allowed: /"); # allow service worker to root context
-
-now error is: 
-An SSL certificate error occurred when fetching the script.
-app.js:299 ServiceWorker registration failed:  
-DOMException: Failed to register a ServiceWorker: 
-An SSL certificate error occurred when fetching the script.
-
-on Chrome Android 4.4.2 https://upload.voxforge1.org/en/read/
-certificate is shown as invalid
-
-on Chrome LInux https://upload.voxforge1.org/en/read/
-certificate is shown as *valid*
-
-
-
-*/
     function uploadZippedSubmission() {
-      if (platform.os.family === "Android" && platform.name === "Chrome Mobile" &&
-          parseInt(platform.os.version) < 5)
-      {
-
-          // Android 4.4.2 Chrome implementation of background sync with service
-          // workers does not like the voxforge1.org SSL certificate
-          // and background sync will not work with service workers, but it still 
-          // tries to perform background syncand returns an error:
-                // An SSL certificate error occurred when fetching the script.
-                // app.js:299 ServiceWorker registration failed:  
-                // DOMException: Failed to register a ServiceWorker: 
-                // An SSL certificate error occurred when fetching the script.
-          // therefore to override this behaviour, tried web worker:
-          // get an net::ERR_CERT_AUTHORITY_INVALID error
-          // Therefore upload from main thread using async call:
-          // now getting same error: net::ERR_CERT_AUTHORITY_INVALID
-          // even though it worked before... uggh!
-
-/* 
-see: https://stackoverflow.com/questions/13862908/ssl-certificate-is-not-trusted-on-mobile-only?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-Put your domain name here: https://www.ssllabs.com/ssltest/analyze.html 
-You should be able to see if there are any issues with your ssl certificate 
-chain. I am guessing that you have SSL chain issues. A short description of the
- problem is that there's actually a list of certificates on your server
- (and not only one) and these need to be in the correct order. If they are
- there but not in the correct order, the website will be fine on desktop 
-browsers (an iOs as well I think), but android is more strict about the order 
-of certificates, and will give an error if the order is incorrect. To fix
- this you just need to re-order the certificates.
-
-see also: https://whatsmychaincert.com/?upload.voxforge1.org
-An error occurred when building the chain for this certificate.  The certificate might lack necessary meta-data or its certificate authority might be malfunctioning.  Details:
-
-* The chain contains an untrusted certificate without standard CA issuer information 
-(subject = "C=US, O=DigiCert Inc, OU=www.digicert.com, CN=Encryption Everywhere DV TLS CA - G2"; 
-issuer = "C=US, O=DigiCert Inc, OU=www.digicert.com, CN=DigiCert Global Root G2"; error code = 20)
-
-https://www.1and1.ca/ssl-checker
-
-
-see: https://superuser.com/questions/452063/the-certificate-is-not-trusted-because-no-issuer-chain-was-provided?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-Some secure web-sites with certificates from trusted authorities have incorrect 
-configuration such that they don't include a chain of intermediate certificates 
-of trust when serving their own certificate.
-
-If you have a system/browser that has seen its share of valid certificates, 
-such intermediaries may already be cached, and you won't be getting any error 
-messages, even if their web-server configuration is still wrong as above.
-
-However, if you're using a freshly installed browser on a fresh system, 
-and such intermediaries weren't cached yet, and the certificate presented
- by the web-server is missing an appropriate chain of intermediaries, then you
- get an error message.
-
-see: https://certlogik.com/ssl-checker/
-*/
-
-           asyncMainThreadUpload(); // still need web workers to perform the zip,and recroding...
-      } else {
-            if (typeof navigator.serviceWorker !== 'undefined') { 
-                navigator.serviceWorker.ready.then(function(swRegistration) { // service workers supported
-                  if (typeof swRegistration.sync !== 'undefined') { 
-                    serviceWorkerUpload(swRegistration); // background sync supported
-                  } else { 
-                    webWorkerUpload(); // background sync not supported
-                  }
-                });
-            } else { // service workers not supported
-              if( !! window.Worker ) { // web workers supported
-                  webWorkerUpload();
-              } else {
-                  asyncMainThreadUpload();
-              }
-
+      if (typeof navigator.serviceWorker !== 'undefined') { 
+          navigator.serviceWorker.ready.then(function(swRegistration) { // service workers supported
+            if (typeof swRegistration.sync !== 'undefined') { 
+              serviceWorkerUpload(swRegistration); // background sync supported
+            } else { 
+              webWorkerUpload(); // background sync not supported
             }
+          });
+      } else { // service workers not supported
+        if( !! window.Worker ) { // web workers supported
+            webWorkerUpload();
+        } else { // should never 
+            asyncMainThreadUpload();
+        }
+
       }
     }
 
     /** 
     * upload submission from main thread, asynchronously...
+    * TODO is this even equired anymore...
     */
     function asyncMainThreadUpload() {
       // TODO make sure not deadlock with service/web workers...
