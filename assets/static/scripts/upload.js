a0effce1324764120082998d50383c4c2e8c4574
Ken MacLean <kmaclean@voxforge.org>
2018-07-27 15:48:12 -0400

    fix case where for each submission made in a session, an additional service worker event listener was added, causing n alter messages for n submissions

--- assets/static/scripts/upload.js
+++ assets/static/scripts/upload.js
@@ -42,6 +42,9 @@ $( window ).unload(function() {
   upload_worker.terminate();
 });
 
+var alert_message;
+var sw_addEventListener_added = false;
+
 /**
 * collect all recorded audio into an array (audioArray) then calls function 
 * that calls web worker that actually creates the zip file for download
@@ -52,11 +55,11 @@ function upload( prompts,
                  speechSubmissionAppVersion, 
                  allClips,
                  language,
-                 alert_message) {
+                 page_alert_message) {
 
     var clipIndex = 0;
     var audioArray = [];
-
+    alert_message = page_alert_message;
     // ### inner functions #################################################
 
     /**
@@ -172,190 +175,199 @@ function upload( prompts,
     function uploadZippedSubmission() {
       return new Promise(function (resolve, reject) {
 
-        if (typeof navigator.serviceWorker !== 'undefined') { 
-            navigator.serviceWorker.ready
-            .then(function(swRegistration) { // service workers supported
-              if (typeof swRegistration.sync !== 'undefined') { 
-                serviceWorkerUpload(swRegistration);  // background sync supported
-              } else { 
-                console.warn('service worker does not support background sync... using web worker');
-                webWorkerUpload(); // background sync not supported
-              }
-            });
-        } else { // service workers not supported
-          if( !! window.Worker ) { // web workers supported
-              webWorkerUpload();
-          } else { // should never get here...
-              asyncMainThreadUpload();
-          }
-        }
-        resolve("OK");
-
-      });
-
-        // ### inner functions #################################################
-        /** 
-        * send message to service worker to start submission upload.
-        *
-        * supposed continue to try to upload even if no Internet, until connection
-        * restablished, and if successful, remove uploaded submission from
-        * browser storage, but this does not seem to work in Windows or Linux, 
-        * only works with Android
-        */
-        function serviceWorkerUpload(swRegistration) {
-          // for processing of return values from service worker, see 
-          // service worker event above (i.e. navigator.serviceWorker.addEventListener... )
-          swRegistration.sync.register('voxforgeSync')
-          .then(function() {
-            console.info('service worker background sync event called - submission will be uploaded shortly');
-           }, function() {
-            console.error('service worker background sync failed, will retry later');
-          });
-        }
-
-        /** 
-        * send message to web worker to upload submission.  If fails, submission
-        * stays in InnoDB until next time user makes submission, and then new
-        * submission and any saved submissions will be uploaded, and removed
-        * from browser storage after successful upload
-        */
-        function webWorkerUpload() {
-            upload_worker.postMessage({
-              command: 'upload',
-              uploadURL: uploadURL,
-            });
-        }
-
-        /** 
-        * upload submission from main thread, asynchronously...
-        * TODO is this even required???
-        * might be useful to allow user to upload manually...
-        */
-        function asyncMainThreadUpload() {
-          // TODO make sure not deadlock with service/web workers...
-          // TODO: should try web workers first...
-          // TODO localize in Read.md page...
-          console.info('submission uploaded (in main thread) asynchronously to VoxForge server');
-
-          processSavedSubmissions()
-          .then(function(result) {
-            console.info('async upload message: ' + result);
-            window.alert( "the following submissions were successfully uploaded " +
-                          "using async procedure: " + result );   
-          })
-          .catch(function(err) {
-            console.error('async upload message: ' + err);
-          });
-        }
-
-        } // uploadZippedSubmission
-
-        // #########################################################################
-
-        /** 
-        * Handler for messages coming from web worker
-        */
-        upload_worker.onmessage = processWorkerEventMessage;
-
-        /** 
-        * Handler for messages coming from service worker
-        */
-        navigator.serviceWorker.addEventListener('message', processWorkerEventMessage);
-
-        /** 
-        * process messages from service worker or web worker
-        */
-        function processWorkerEventMessage(event) {
-            var returnObj = event.data;
-            console.log(returnObj.workertype + " says: " + returnObj.status);
-
-            var workertype;
-            if (returnObj.workertype == "serviceworker") {
-              workertype = alert_message.serviceworker;
-            } else if (returnObj.workertype == "webworker") {
-              workertype = alert_message.webworker;
-            } else {
-              workertype = alert_message.workernotfound;
-            }
-
-            function processMessage(m) {
-                console.info(workertype + ": " + m);
-                Promise.all(promise_list) // if user recording, wait for stop click before displaying alert
-                .then(function() {
-                  window.alert(m);
+            if (typeof navigator.serviceWorker !== 'undefined') { 
+                navigator.serviceWorker.ready
+                .then(function(swRegistration) { // service workers supported
+                  if (typeof swRegistration.sync !== 'undefined') { 
+                    serviceWorkerUpload(swRegistration);  // background sync supported
+                  } else { 
+                    console.warn('service worker does not support background sync... using web worker');
+                    webWorkerUpload(); // background sync not supported
+                  }
                 });
+            } else { // service workers not supported
+              if( !! window.Worker ) { // web workers supported
+                  webWorkerUpload();
+              } else { // should never get here...
+                  asyncMainThreadUpload();
+              }
             }
+            resolve("OK");
 
-            switch (returnObj.status) {
-              case 'AllUploaded':
-                var filesUploaded = returnObj.filesUploaded;
-                // TODO too late... profile has already been saved to localstorage
-                profile.addToNumSubmissonsUploaded( filesUploaded.length );
-                var submissionText = (filesUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
-                processMessage( filesUploaded.length + " " + 
-                                submissionText + " " +
-                                alert_message.uploaded_message  + "\n    " +
-                                filesUploaded.join("\n    ")
-                );
-                break;
+        }); // Promise
+      } // uploadZippedSubmission
 
-              case 'noneUploaded': // files saved to browser storage
-                var filesNotUploaded =  returnObj.filesNotUploaded;
-                var submissionText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
-                processMessage(  alert_message.localstorage_message + "\n" +
-                                 alert_message.browsercontains_message.trim() + " " + // remove newline
-                                 filesNotUploaded.length + " " + 
-                                 submissionText + ":\n    " + 
-                                 filesNotUploaded.join("\n    ")
-                );
-                break;
+      // ### inner functions #################################################
+      /** 
+      * send message to service worker to start submission upload.
+      *
+      * supposed continue to try to upload even if no Internet, until connection
+      * restablished, and if successful, remove uploaded submission from
+      * browser storage, but this does not seem to work in Windows or Linux, 
+      * only works with Android
+      */
+      function serviceWorkerUpload(swRegistration) {
+        // for processing of return values from service worker, see 
+        // service worker event above (i.e. navigator.serviceWorker.addEventListener... )
+        swRegistration.sync.register('voxforgeSync')
+        .then(function() {
+          console.info('service worker background sync event called - submission will be uploaded shortly');
+         }, function() {
+          console.error('service worker background sync failed, will retry later');
+        });
+      }
 
-              // if there is an error with one submission (usually server side check - e.g.
-              // file too big for server settings), then other submissions will upload, but
-              // erroneous one will stay in browser storage.
-              // TODO need a way for user to save these their o/s filesystem and upload
-              // them to VoxForge server some other way.
-              case 'partialUpload':
-                var filesNotUploaded = returnObj.filesNotUploaded;
-                var filesUploaded = returnObj.filesUploaded;
-                profile.addToNumSubmissonsUploaded( filesUploaded.length );
+      /** 
+      * send message to web worker to upload submission.  If fails, submission
+      * stays in InnoDB until next time user makes submission, and then new
+      * submission and any saved submissions will be uploaded, and removed
+      * from browser storage after successful upload
+      */
+      function webWorkerUpload() {
+          upload_worker.postMessage({
+            command: 'upload',
+            uploadURL: uploadURL,
+          });
+      }
 
-                var savedText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
-                var uploadedText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
+      /** 
+      * upload submission from main thread, asynchronously...
+      * TODO is this even required???
+      * might be useful to allow user to upload manually...
+      */
+      function asyncMainThreadUpload() {
+        // TODO make sure not deadlock with service/web workers...
+        // TODO: should try web workers first...
+        // TODO localize in Read.md page...
+        console.info('submission uploaded (in main thread) asynchronously to VoxForge server');
 
-                var m = "Partial Upload:\n\n" +
-                      filesUploaded.length + " " + 
-                      savedText + " " +
-                      alert_message.uploaded_message + 
-                      "    " + filesUploaded.join("\n    ") +
-                      "\n========================\n" +
-                      alert_message.browsercontains_message.trim() + " " + // removes newline
-                      filesNotUploaded.length + " " + 
-                      uploadedText + ":\n" + 
-                      "    " + filesNotUploaded.join("\n    ");
-                if (returnObj.err) {
-                    m = m + "\n========================\n";
-                    m = m + "\n\nserver error message: " + returnObj.err;
-                }
-                processMessage(m);
-                break;
+        processSavedSubmissions()
+        .then(function(result) {
+          console.info('async upload message: ' + result);
+          window.alert( "the following submissions were successfully uploaded " +
+                        "using async procedure: " + result );   
+        })
+        .catch(function(err) {
+          console.error('async upload message: ' + err);
+        });
+      }
 
-              default:
-                console.error('message from upload worker: transfer error: ' +
-                              returnObj.status + " " + returnObj.message);
+      /** 
+      * process messages from service worker or web worker
+      *
+      * TODO: Needs to be outside upload function, otherwise with second submission, 
+      * get a 2 upload messages, on third submission, 3 upload messages for 
+      * same submission, and so on.... not sure why
+      */
+      function processWorkerEventMessage(event) {
+          var returnObj = event.data;
+          console.log(returnObj.workertype + " says: " + returnObj.status);
+
+          var workertype;
+          if (returnObj.workertype == "serviceworker") {
+            workertype = alert_message.serviceworker;
+          } else if (returnObj.workertype == "webworker") {
+            workertype = alert_message.webworker;
+          } else {
+            workertype = alert_message.workernotfound;
           }
-        }
+
+          function processMessage(m) {
+              console.info(workertype + ": " + m);
+              Promise.all(promise_list) // if user recording, wait for stop click before displaying alert
+              .then(function() {
+                window.alert(m);
+              });
+          }
+
+          switch (returnObj.status) {
+            case 'AllUploaded':
+              var filesUploaded = returnObj.filesUploaded;
+              // TODO too late... profile has already been saved to localstorage
+              //profile.addToNumSubmissonsUploaded( filesUploaded.length );
+              var submissionText = (filesUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
+              processMessage( filesUploaded.length + " " + 
+                              submissionText + " " +
+                              alert_message.uploaded_message  + "\n    " +
+                              filesUploaded.join("\n    ")
+              );
+
+              break;
+
+            case 'noneUploaded': // files saved to browser storage
+              var filesNotUploaded =  returnObj.filesNotUploaded;
+              var submissionText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
+              processMessage(  alert_message.localstorage_message + "\n" +
+                               alert_message.browsercontains_message.trim() + " " + // remove newline
+                               filesNotUploaded.length + " " + 
+                               submissionText + ":\n    " + 
+                               filesNotUploaded.join("\n    ")
+              );
+              break;
+
+            // if there is an error with one submission (usually server side check - e.g.
+            // file too big for server settings), then other submissions will upload, but
+            // erroneous one will stay in browser storage.
+            // TODO need a way for user to save these their o/s filesystem and upload
+            // them to VoxForge server some other way.
+            case 'partialUpload':
+              var filesNotUploaded = returnObj.filesNotUploaded;
+              var filesUploaded = returnObj.filesUploaded;
+              //profile.addToNumSubmissonsUploaded( filesUploaded.length );
+
+              var savedText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
+              var uploadedText = (filesNotUploaded.length > 1 ? alert_message.submission_plural : alert_message.submission_singular);
+
+              var m = "Partial Upload:\n\n" +
+                    filesUploaded.length + " " + 
+                    savedText + " " +
+                    alert_message.uploaded_message + 
+                    "    " + filesUploaded.join("\n    ") +
+                    "\n========================\n" +
+                    alert_message.browsercontains_message.trim() + " " + // removes newline
+                    filesNotUploaded.length + " " + 
+                    uploadedText + ":\n" + 
+                    "    " + filesNotUploaded.join("\n    ");
+              if (returnObj.err) {
+                  m = m + "\n========================\n";
+                  m = m + "\n\nserver error message: " + returnObj.err;
+              }
+              processMessage(m);
+              break;
+
+            default:
+              console.error('message from upload worker: transfer error: ' +
+                            returnObj.status + " " + returnObj.message);
+        } // switch
+      } // processWorkerEventMessage
 
       // #########################################################################
-      // return of promise must be after worker handler creation
 
+      /** 
+      * Handler for messages coming from web worker
+      */
+      upload_worker.onmessage = processWorkerEventMessage;
+
+      /** 
+      * Handler for messages coming from service worker
+      * every time upload is called, it adds a new event listener...
+      */
+      if ( ! sw_addEventListener_added) {
+        navigator.serviceWorker.addEventListener('message', processWorkerEventMessage);
+        sw_addEventListener_added = true;
+      }
+
+      // return of promise must be after worker handler creation
       return new Promise(function (resolve, reject) {
         processAudio()
         .then(callWorker2createZipFile)
         .then(uploadZippedSubmission)
-        .then(function () { // TODO is this even required??? creating a blocking wait
-          resolve(); // to resolve the returned promise
-        });
+//        .then(function () { // TODO is this even required??? creating a blocking wait
+//          resolve(); // to resolve the returned promise
+//        });
+        .then(resolve);
       });
 }
+
+
 
