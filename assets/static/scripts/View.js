a8414fc866becae94e2b32c20077470281c3e546
Ken MacLean <kmaclean@voxforge.org>
2019-01-24 19:56:35 -0500

    more refactor of View class - make smaller methods

--- assets/static/scripts/View.js
+++ assets/static/scripts/View.js
@@ -28,205 +28,84 @@ function View (parms,
     this.parms = parms;
     this.prompts = prompts;
     this.profile = profile;
-    this.location = location;
-
+    this.pageVariables = pageVariables;
     this.displayWaveform = parms.displayWaveform;
+    
+    this._init();
+}
 
-    // buttons
+View.prototype._init = function () {
+    this._setupButtons();
+
+    // TODO this might work with delete if used class syntax (which should
+    // pick up many elements) instead of id synstax (which only pick up one element)
+    this.delete_clicked = document.querySelector('#delete_clicked'); // only picks up first instance of in=delete in DOM
+
+    this._setupTranslations();
+    this._instantiateClassDependencies();
+}
+
+View.prototype._setupButtons = function () {
     this.record = document.querySelector('.record');
     this.stop = document.querySelector('.stop');
     this.upload = document.querySelector('.upload');
+}
 
-    // TODO this might work with delete if used class syntax (which should pick up many elements) instead of id synstax (which only pick up one element)
-    this.delete_clicked = document.querySelector('#delete_clicked'); // only picks up first instance of in=delete in DOM
+View.prototype._setupTranslations = function () {
+    this.localized_yes = this.pageVariables.localized_yes;
+    this.localized_no = this.pageVariables.localized_no;
+    this.localized_other = this.pageVariables.localized_other;
+    this.please_select = this.pageVariables.please_select;
+    this.default_value = this.pageVariables.default_value;
+    this.alert_message = this.pageVariables.alert_message;
 
-    this.localized_yes = pageVariables.localized_yes;
-    this.localized_no = pageVariables.localized_no;
-    this.localized_other = pageVariables.localized_other;
-    this.please_select = pageVariables.please_select;
-    this.default_value = pageVariables.default_value;
-    this.alert_message = pageVariables.alert_message;
+    this.playbuttontext = this.pageVariables.playbuttontext;
+    this.stopbuttontext = this.pageVariables.stopbuttontext;
+}
 
-    this.playbuttontext = pageVariables.playbuttontext;
-    this.stopbuttontext = pageVariables.stopbuttontext;
-
+View.prototype._instantiateClassDependencies = function () {
     this.settings = new Settings();
     this.submissionsLog = new SubmissionsLog(
-         pageVariables.saved_submissions,
-         pageVariables.uploaded_submissions,
+         this.pageVariables.saved_submissions,
+         this.pageVariables.uploaded_submissions,
     );
     this.audioPlayer = new AudioPlayer(
-        prompts,
-        pageVariables,
-    );     
+        this.prompts,
+        this.pageVariables,
+    ); 
 }
 
 // ### METHODS #################################################################
 
 /** 
 * Initialize object with async operations (returns a promise)
+*
+* see: https://stackoverflow.com/questions/7694501/class-vs-static-method-in-javascript
 */
 View.prototype.init = function () {
     var self = this;
-
-    /**
-    * The value of contents of the independent_div is compared to the passed in 
-    * value, and if they are equal, then the dependent_div is displayed, otherwise
-    * it is hidden 
-    *
-    * showDivBasedonValue makes the view of one div dependent on the value of a select 
-    * field in another div, and attaches an event handler to independent div so that
-    * any changes in it are reflected in dependent div
-    *
-    * see https://stackoverflow.com/questions/15566999/how-to-show-form-input-fields-based-on-select-value
-    */
-    var showDivBasedonValue = function (independent_div, value, dependent_div, handler_already_created) {
-      function test ( boolean_result ) {
-        if( boolean_result ){
-          $(dependent_div).show();
-        } else {
-          $(dependent_div).hide();
-        }
-      }
-
-      if ( typeof(value) === "boolean" && value === true ) { 
-        // show if false; hide if true
-        test( ! $(independent_div).val() );
-      } else {
-        test( $(independent_div).val()===value );
-      }
-
-      // only need to create event handler on first call to this function
-      if ( ! handler_already_created ) 
-      {
-        $(independent_div).change(function () { // creates an event handler
-            showDivBasedonValue(independent_div, value, dependent_div, true); 
-        } );
-      }
-    }
-
-    /*
-     * compare value of independent div with passed in value and if equal, reset
-     * selection option to default in dependent div
-     */
-    function setDefault(independent_div, value, dependent_div, handler_already_created) {
-       if ( $(independent_div).val() === value ) {
-           $(dependent_div).val($("select option:first").val()).change();
-       }
-      // only need to create event handler on first call to this function
-      if ( ! handler_already_created ) 
-      {
-        $(independent_div).change(function () { // creates an event handler
-            setDefault(independent_div, value, dependent_div, true); 
-        } );
-      }       
-    }
-
-    showDivBasedonValue('#native_speaker', self.localized_no, '#first_language_display', false);
-    showDivBasedonValue('#native_speaker', self.localized_yes, '#dialect_display', false);
-    // causes sub-dialect to display immediately rather than when Canadian or American dialect selected
-    //showDivBasedonValue('#native_speaker', self.localized_yes, '#sub_dialect_display', false);
-    setDefault('#native_speaker', self.localized_yes, '#first_language', false);
-    setDefault('#native_speaker', self.localized_no, '#dialect', false);
-    setDefault('#native_speaker', self.localized_no, '#sub_dialect', false);
     
-    showDivBasedonValue('#first_language', self.localized_other, '#first_language_other_display', false);
+    this._showDivBasedonValue('#native_speaker', this.localized_no, '#first_language_display', false);
+    this._showDivBasedonValue('#native_speaker', this.localized_yes, '#dialect_display', false);
+    // causes sub-dialect to display immediately rather than when Canadian or American dialect selected
+    //showDivBasedonValue('#native_speaker', this.localized_yes, '#sub_dialect_display', false);
+    this._setDefault('#native_speaker', this.localized_yes, '#first_language', false);
+    this._setDefault('#native_speaker', this.localized_no, '#dialect', false);
+    this._setDefault('#native_speaker', this.localized_no, '#sub_dialect', false);
+    
+    this._showDivBasedonValue('#first_language', this.localized_other, '#first_language_other_display', false);
     // true means hide if there is something in the username field
-    showDivBasedonValue('#username', true, '#anonymous_instructions_display', false);
-    showDivBasedonValue('#microphone', self.localized_other, '#microphone_other_display', false);
-    showDivBasedonValue('#dialect', self.localized_other, '#dialect_other_display', false);
-    showDivBasedonValue('#recording_location', self.localized_other, '#recording_location_other_display', false);
-    showDivBasedonValue('#background_noise', self.localized_yes, '#background_noise_display', false);
-    showDivBasedonValue('#noise_type', self.localized_other, '#noise_type_other_display', false);
+    this._showDivBasedonValue('#username', true, '#anonymous_instructions_display', false);
+    this._showDivBasedonValue('#microphone', this.localized_other, '#microphone_other_display', false);
+    this._showDivBasedonValue('#dialect', this.localized_other, '#dialect_other_display', false);
+    this._showDivBasedonValue('#recording_location', this.localized_other, '#recording_location_other_display', false);
+    this._showDivBasedonValue('#background_noise', this.localized_yes, '#background_noise_display', false);
+    this._showDivBasedonValue('#noise_type', this.localized_other, '#noise_type_other_display', false);
 
-    /**
-    *
-    * see: https://stackoverflow.com/questions/7694501/class-vs-static-method-in-javascript
-    */
+    this._setDependentSelect( $('#dialect'), $('#sub_dialect'), $("#sub_dialect_display") );
 
-    /**
-    * This function changes the contents of a dependent select list based on the
-    * contents of a independent select list.  This is used to set the 
-    * contents of the dependent sub-dialect selection list based on the value
-    * the independent dialect selection list.
-    *
-    * Read.md contains the entire list of possible subdialects for a language,
-    * this function filters those contents based on what is contained in dialect
-    * so that user only sees filtered results
-    *
-    * see https://stackoverflow.com/questions/10570904/use-jquery-to-change-a-second-select-list-based-on-the-first-select-list-option
-    * Store all #subdialect's options in a variable, filter them according 
-    * to the value of the chosen option in #dialect, and set them using 
-    * .html() in #subdialect:
-    */
-    // TODO when only one optgroup, first selection is not immediately selectable
-    // need to select second or third option, then can select first option
-    function setDependentSelect($independent, $dependent, $dependent_display) {
-        var $optgroup = $dependent.find( 'optgroup' );
-        var $selected = $dependent.find( ':selected' );
-
-        $independent.on( 'change', function() {
-            var filter =  $optgroup.filter( '[name="' + this.value + '"]' );
-
-            if ( filter.length ) {
-              filter = filter.add( $selected );
-              $dependent_display.show();
-              $dependent.html( filter );
-            }
-            else
-            {
-              $dependent_display.hide();
-            }
-            $dependent.val(self.default_value);                 
-            $dependent.prop('defaultSelected');
-       
-        } ).trigger('change');
-    }
-
-    setDependentSelect( $('#dialect'), $('#sub_dialect'), $("#sub_dialect_display") );
-
-    /**
-    * fill other languages select list with stringified array the names of most 
-    * ISO 639-1 language names
-    */
-    var langscodes = languages.getAllLanguageCode(); // array of language codes
-    //var option = ''; // string
-    var option = '<option value="' + self.default_value + '">'+ self.please_select + '</option>';
-    for (var i=1;i<langscodes.length;i++){
-       option += '<option value="'+ langscodes[i] + '">' +
-       languages.getLanguageInfo(langscodes[i]).name + " (" +
-       languages.getLanguageInfo(langscodes[i]).nativeName + ")" +
-       '</option>';
-    }
-    option += '<option value="' + self.localized_other + '">' + self.localized_other + '</option>'; 
-    $('#first_language').append(option);
-
-    // Prompts
-
-    this.maxnumpromptschanged = document.querySelector('#max_num_prompts');
-
-    // set default (device dependent) max number of prompts the user can record 
-    var option = ''; // clear previous use of option var
-    if (self.prompts.max_numPrompts_selector > 10) {
-        var startPrompt = 10; // min number of prompts no matter what device
-        var incr = 5;
-        for (var i=startPrompt; i <= self.prompts.max_numPrompts_selector; i = i + incr){
-           option += '<option value="'+ i + '">' + i +  '</option>';
-        }
-        $('#max_num_prompts').append(option);
-        $('#max_num_prompts-display').show();
-    } else {
-        $('#max_num_prompts-display').hide();
-    }
-    /**
-    * updates the current number of prompts that the user selected from dropdown
-    */
-    //$('#max_num_prompts').click(function () { 
-    $('#max_num_prompts').change(function () { 
-      self.prompts.userChangedMaxNum( this.value.replace(/[^0-9\.]/g,'') );
-      self.updateProgress();
-    });
-
+    this._setupLanguages();
+    this._setupPrompts();
     this.settings.initPopup();
     this.submissionsLog.setupDisplay();
 
@@ -248,20 +127,164 @@ View.prototype.init = function () {
     }); // promise
 }
 
-/** 
-* display upload to VoxForge server status to user
+/**
+* The value of contents of the independent_div is compared to the passed in 
+* value, and if they are equal, then the dependent_div is displayed, otherwise
+* it is hidden 
 *
-* TODO no longer used
+* showDivBasedonValue makes the view of one div dependent on the value of a select 
+* field in another div, and attaches an event handler to independent div so that
+* any changes in it are reflected in dependent div
+*
+* see https://stackoverflow.com/questions/15566999/how-to-show-form-input-fields-based-on-select-value
 */
-View.prototype.showUploadStatus = function (message) {
-    $('#upload_status_display').show();
-    $('#upload_status_display').text(message);
-    $('#upload_status_display').css({ 'color': 'green', 'font-size': '50%' });
-    setTimeout( function () {
-      //document.querySelector('.upload_status_display').innerText = "";
-      $('#upload_status_display').hide();
-      return;
-    }, 3000);
+View.prototype._showDivBasedonValue = function (
+    independent_div,
+    value,
+    dependent_div,
+    handler_already_created)
+{
+    var self = this;
+    
+    function test ( boolean_result ) {
+        if( boolean_result ){
+          $(dependent_div).show();
+        } else {
+          $(dependent_div).hide();
+        }
+    }
+
+    if ( typeof(value) === "boolean" && value === true ) { 
+        // show if false; hide if true
+        test( ! $(independent_div).val() );
+    } else {
+        test( $(independent_div).val()===value );
+    }
+
+    // only need to create event handler on first call to this function
+    if ( ! handler_already_created )  {
+        $(independent_div).change(function () { // creates an event handler
+            self._showDivBasedonValue(independent_div, value, dependent_div, true); 
+        } );
+    }
+}
+
+/*
+ * compare value of independent div with passed in value and if equal, reset
+ * selection option to default in dependent div
+ */
+View.prototype._setDefault = function (
+    independent_div,
+    value,
+    dependent_div,
+    handler_already_created)
+{
+    var self = this;
+    
+    if ( $(independent_div).val() === value ) {
+       $(dependent_div).val($("select option:first").val()).change();
+    }
+    // only need to create event handler on first call to this function
+    if ( ! handler_already_created ) {
+        $(independent_div).change(function () { // creates an event handler
+            self._setDefault(independent_div, value, dependent_div, true); 
+        } );
+    }       
+}
+
+/**
+* This function changes the contents of a dependent select list based on the
+* contents of a independent select list.  This is used to set the 
+* contents of the dependent sub-dialect selection list based on the value
+* the independent dialect selection list.
+*
+* Read.md contains the entire list of possible subdialects for a language,
+* this function filters those contents based on what is contained in dialect
+* so that user only sees filtered results
+*
+* see https://stackoverflow.com/questions/10570904/use-jquery-to-change-a-second-select-list-based-on-the-first-select-list-option
+* Store all #subdialect's options in a variable, filter them according 
+* to the value of the chosen option in #dialect, and set them using 
+* .html() in #subdialect:
+*/
+// TODO when only one optgroup, first selection is not immediately selectable
+// need to select second or third option, then can select first option
+View.prototype._setDependentSelect = function (
+    $independent,
+    $dependent,
+    $dependent_display)
+{
+    var $optgroup = $dependent.find( 'optgroup' );
+    var $selected = $dependent.find( ':selected' );
+
+    $independent.on( 'change', function() {
+        var filter =  $optgroup.filter( '[name="' + this.value + '"]' );
+
+        if ( filter.length ) {
+          filter = filter.add( $selected );
+          $dependent_display.show();
+          $dependent.html( filter );
+        }
+        else
+        {
+          $dependent_display.hide();
+        }
+        $dependent.val(self.default_value);                 
+        $dependent.prop('defaultSelected');
+   
+    } ).trigger('change');
+}
+
+/**
+* fill other languages select list with stringified array the names of most 
+* ISO 639-1 language names
+*/
+View.prototype._setupLanguages = function () {
+    var langscodes = languages.getAllLanguageCode(); // array of language codes
+    var option = '<option value="' + this.default_value + '">'+ this.please_select + '</option>';
+    for (var i=1;i<langscodes.length;i++){
+       option += '<option value="'+ langscodes[i] + '">' +
+       languages.getLanguageInfo(langscodes[i]).name + " (" +
+       languages.getLanguageInfo(langscodes[i]).nativeName + ")" +
+       '</option>';
+    }
+    option += '<option value="' + this.localized_other + '">' + this.localized_other + '</option>'; 
+    $('#first_language').append(option);
+}
+
+/**
+* 
+*/
+View.prototype._setupPrompts = function () {
+    var self = this;
+    
+    this.maxnumpromptschanged = document.querySelector('#max_num_prompts');
+
+    if (this.prompts.max_numPrompts_selector > 10) {
+        this._displayPrompts();
+    } else {
+        $('#max_num_prompts-display').hide();
+    }
+    /**
+    * updates the current number of prompts that the user selected from dropdown
+    */
+    //$('#max_num_prompts').click(function () { 
+    $('#max_num_prompts').change(function () { 
+      self.prompts.userChangedMaxNum( this.value.replace(/[^0-9\.]/g,'') );
+      self.updateProgress();
+    });
+}
+
+// set default (device dependent) max number of prompts the user can record 
+View.prototype._displayPrompts = function () {
+    var startPrompt = 10; // min number of prompts no matter what device
+    var incr = 5;
+    var option = ''; // clear previous use of option var    
+    for (var i=startPrompt; i <= this.prompts.max_numPrompts_selector; i = i + incr){
+       option += '<option value="'+ i + '">' + i +  '</option>';
+    }
+    $('#max_num_prompts').append(option);
+    $('#max_num_prompts-display').show();
 }
 
 /**
